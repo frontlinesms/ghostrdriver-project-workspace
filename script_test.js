9daa7102ae62f000a428d0c24318b777523a3ff7
Alex Anderson <alex@frontlinesms.com>
2012-05-28 08:31:19 +0300

    Added new runner; implemented some cookie testing.

--- script_test.js
+++ script_test.js
@@ -1,5 +1,21 @@
-var testScript = function() {
+var testScript = function(page) {
 	/* WRITE CUSTOM SCRIPT HERE */
+	var cookieString = page.evaluate(function() {
+		return document.cookie;
+	});
+	console.log("COOKIES: " + cookieString);
+
+	var cookies = [];
+	var p = cookieString.split(";");
+	for(i=p.length-1;i>=0;--i) {
+		var q = p[i].split("=");
+		cookies.push({key:q[0], value:q[1]});
+	}
+
+	for(i=0;i<cookies.length;++i) {
+		var cookie = cookies[i];
+		console.log("Cookie: " + cookie.key + "->" + cookie.value);
+	}
 }
 
 console.log('Loading a web page');
@@ -8,7 +24,7 @@ var url = "http://www.phantomjs.org/";
 page.open(url, function (status) {
 	//Page is loaded!
 	console.log("CALLING TEST SCRIPT...!");
-	testScript();
+	testScript(page);
 	console.log("TEST SCRIPT COMPLETED");
 	phantom.exit();
 });
